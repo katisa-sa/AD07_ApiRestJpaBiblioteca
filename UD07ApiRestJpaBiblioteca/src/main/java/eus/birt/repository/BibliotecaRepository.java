351349c4b06a1e1910bbcecb32909f53fa77f1d7
Katerin Sanz Celorrio <ksanz@birt.eus>
2025-05-16 20:51:24 +0200

    commit 2

--- UD07ApiRestJpaBiblioteca/src/main/java/eus/birt/repository/BibliotecaRepository.java
+++
@@ -1,9 +0,0 @@
-package eus.birt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import eus.birt.domain.Biblioteca;
-
-public interface BibliotecaRepository extends JpaRepository<Biblioteca,Long>{
-
-}
